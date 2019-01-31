4c947e5e06153e9693733655b1c1955c580de53c
Grace Hartzell <gracehartzell@utexas.edu>
2019-01-31 14:23:20 -0600

    Trying to get data

--- server/scraper/yummly.js
+++ server/scraper/yummly.js
@@ -12,16 +12,20 @@ async function run() {
   const SHOP_SELECTOR = '#basketfulDefaultButton';
   await page.waitForSelector(SHOP_SELECTOR);
   await page.click(SHOP_SELECTOR);
-  // Selector for heb. the id is store id.
-  // 'button[data-selectedretailerid="a63147ae-67f4-4e71-97ce-9dff0582ef99"]'
 
-  // Once you have list of ingredients on heb.
-  // Query for all <tr>
+  // const HEB_SELECTOR =
+  // 'button[data-selectedretailerid="a63147ae-67f4-4e71-97ce-9dff0582ef99]';
 
-  const rows = document.querySelectorAll('tr');
+  // await page.waitForSelector(HEB_SELECTOR);
+  // await page.click(HEB_SELECTOR);
+
+  const rows = await page.evaluate(() =>
+    Array.from(document.querySelectorAll('tr').innerHTML),
+  ); // returns [object Promise]
+  console.log(`HERE ARE THE ROWS: ${rows}, TYPEOF: ${typeof rows}`); // rows = object
+
   rows.forEach(row => {
     // Row is going to be a node. Specifically a tr element.
-    // Step one. Get the second td.
     const children = row.childNodes;
     if (children.length > 1) {
       const targetTd = children && children[1];
