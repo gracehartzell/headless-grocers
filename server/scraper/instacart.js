3542748bc90928839129a79a89ee8a2c89020faf
Grace Hartzell <gracehartzell@utexas.edu>
2019-01-31 19:09:56 -0600

    More flexible search

--- server/scraper/instacart.js
+++ server/scraper/instacart.js
@@ -27,13 +27,22 @@ async function run() {
   await page.click(BUTTON_SELECTOR);
   await page.waitForNavigation();
 
-  const itemToSearch = 'banana';
-  const searchURL = `https://www.instacart.com/store/h-e-b/search_v3/${itemToSearch}`;
-
+  const searchURL = `https://www.instacart.com/store/h-e-b/search_v3/`;
   await page.goto(searchURL);
 
+  const SEARCH_INPUT_SELECTOR =
+    '#header > div > div > div.header-hero.no-hero-image > div.header-hero-inner-wrapper > div.rmq-550d79d6.rmq-48fdb2da > form > div > input[type="text"]';
+  const SEARCH_BUTTON_SELECTOR =
+    '#header > div > div > div.header-hero.no-hero-image > div.header-hero-inner-wrapper > div.rmq-550d79d6.rmq-48fdb2da > form > div > button';
+  await page.waitForSelector(SEARCH_INPUT_SELECTOR);
+  await page.click(SEARCH_INPUT_SELECTOR);
+  await page.keyboard.type('bread');
+  await page.click(SEARCH_BUTTON_SELECTOR);
+
+  await page.waitForNavigation();
   const ADD_SELECTOR =
-    '#search_grid_a5569d1fffa3d9fa6ae1873b08eaec23-module > ul > li.item-card.in-cart > div > span > span > button';
+    '#search_grid_a5569d1fffa3d9fa6ae1873b08eaec23-module > ul > li:nth-child(1) > div > span > span > button';
+  await page.waitForSelector(ADD_SELECTOR);
   await page.click(ADD_SELECTOR);
 
   // browser.close();
